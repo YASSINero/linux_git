0a07331d8ed3660d00436aa5ecbd32a887c89a75
YASSINero <afriadeamassine@gmail.com>
2023-08-29 22:53:11 +0100

    added js script

---
+++ script/behaviour.js
@@ -0,0 +1,17 @@
+const my_int = 20;
+const my_float = 20.05;
+
+console.log("My int: " + my_int + '\n');
+console.log("My float: " + my_float + '\n');
+console.log("My float: " + my_float + 5);
+
+
+const my_phrase = ("My float: " + my_float + '\n');
+
+console.log(typeof my_phrase);
+console.log(typeof my_float);
+console.log("=====d=o=n=t==m=i=n=d==m=e=====");
+
+console.log("2.5*2" == 5);
+
+
